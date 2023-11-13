9b033238614dde5d0223ab47df4d86c47d7b4752
Rodrigo <rodrigo.hdez.oficial@gmail.com>
2023-11-13 12:51:21 +0100

    Imlementacion de dinamismo

--- app/src/main/java/com/mcas2/firstdesign/Login.java
+++ app/src/main/java/com/mcas2/firstdesign/Login.java
@@ -1,16 +1,25 @@
 package com.mcas2.firstdesign;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import static android.app.PendingIntent.getActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Login extends AppCompatActivity {
 
@@ -19,10 +28,37 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        EditText user = findViewById(R.id.user);
+        EditText password = findViewById(R.id.password);
+        CheckBox remember = findViewById(R.id.recuerdame);
+
         TextView olvideContrasena = findViewById(R.id.olvideContrasena);
         SpannableString ForgotPassword = new SpannableString("Olvidé mi contraseña");
         ForgotPassword.setSpan(new UnderlineSpan(), 0, ForgotPassword.length(), 0);
         olvideContrasena.setText(ForgotPassword);
+        olvideContrasena.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                builder.setMessage("Si has olvidado tu contraseña, te jodes, lo siento")
+                        .setCancelable(false)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Toast.makeText(Login.this, "No te preocupes, siempre puedes suicidarte", Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .setNegativeButton("CTM", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                ImageView huella = findViewById(R.id.logoLogin);
+                                Animation DenyAnim = AnimationUtils.loadAnimation(Login.this, R.anim.deny);
+                                huella.startAnimation(DenyAnim);
+                                dialog.cancel();
+                            }
+                        });
+                builder.create();
+                builder.show();
+            }
+        });
 
         ImageView img = findViewById(R.id.background);
         img.setAlpha(0.1f);
@@ -30,5 +66,43 @@ public class Login extends AppCompatActivity {
                 .load("https://images.unsplash.com/photo-1533240332313-0db49b459ad6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80")
                 .centerCrop()
                 .into(img);
+
+        Button logIn = findViewById(R.id.login);
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(user.getText().toString().isEmpty() || password.getText().toString().isEmpty()){
+                    ImageView huella = findViewById(R.id.logoLogin);
+                    Animation DenyAnim = AnimationUtils.loadAnimation(Login.this, R.anim.deny);
+                    huella.startAnimation(DenyAnim);
+                }else{
+                    if(!remember.isChecked()){
+                        AlertDialog.Builder builderDatos = new AlertDialog.Builder(Login.this);
+                        builderDatos.setMessage("¿Quieres guardar la contraseña?")
+                                .setCancelable(false)
+                                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        Toast.makeText(Login.this, "Contarseña guardada", Toast.LENGTH_LONG).show();
+                                    }
+                                })
+                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        Toast.makeText(Login.this, "No has guardado tu contraseña", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                        builderDatos.create();
+                        builderDatos.show();
+                    }else{
+                        Toast.makeText(Login.this, "Contraseña guardada", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                user.setText("");
+                password.setText("");
+            }
+
+
+        });
+        TextView copy = findViewById(R.id.copyRight);
+        copy.setText("Hikers by Rodrigo Hernández\n");
     }
 }
